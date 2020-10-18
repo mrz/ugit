37c444f5969654d6e5c4e95d891fbf4af2bf2cc3
Mattia Ziulu <mziulu@gmail.com>
2020-10-18 15:08:16 +0200

    [step 1] cli: Add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,24 @@
+use clap::{App, Arg};
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+
 fn main() {
+    let matches = App::new(NAME)
+        .version(VERSION)
+        .about("DIY Git")
+        .arg(
+            Arg::new("init")
+                .about("Initialize ugit repository")
+                .index(1),
+        )
+        .get_matches();
+
+    if let Some(_) = matches.value_of("init") {
+        init();
+    }
+}
+
+fn init() {
+    println!("Init");
 }

194614f8d72c41607288f24dbcc268c40e6b0ec5
Mattia Ziulu <mziulu@gmail.com>
2020-10-18 18:02:13 +0200

    [step 3] hash-object: Save object

--- src/data.rs
+++ src/data.rs
@@ -1,9 +1,32 @@
-use std::io;
+use hex;
+use io::Result;
+use sha1::{Digest, Sha1};
+use std::io::prelude::*;
+use std::path::Path;
+use std::{fs, io};
 
 pub const GIT_DIR: &'static str = ".ugit";
 
-pub fn init() -> io::Result<()> {
+pub fn init() -> Result<()> {
     std::fs::create_dir(GIT_DIR)?;
+
+    Ok(())
+}
+
+pub fn hash_object(pathspecs: Vec<&str>) -> Result<()> {
+    for pathspec in pathspecs.iter() {
+        let contents = fs::read(pathspec)?;
+
+        let mut hasher = Sha1::new();
+        hasher.update(&contents);
+
+        let result = hasher.finalize();
+
+        let path = Path::new(GIT_DIR).join(hex::encode(&result));
+
+        let mut f = fs::File::create(path)?;
+        f.write_all(&contents)?;
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
+use std::env;
+use std::io::Result;
+
 use clap::{App, Arg};
-use std::{env, io};
 
 mod data;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const NAME: &'static str = env!("CARGO_PKG_NAME");
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let matches = App::new(NAME)
         .version(VERSION)
         .about("DIY Git")
-        .arg(
-            Arg::new("init")
-                .about("Initialize ugit repository")
-                .index(1),
+        .subcommand(App::new("init").about("Initialize ugit repository"))
+        .subcommand(
+            App::new("hash-object")
+                .about("Save a file in ugit object database")
+                .arg(Arg::new("pathspec").takes_value(true).multiple(true)),
         )
         .get_matches();
 
-    if let Some(_) = matches.value_of("init") {
-        init()?;
-        println!(
-            "Initialized empty ugit repository in {}/{}",
-            env::current_dir()?.display(),
-            data::GIT_DIR
-        );
+    match matches.subcommand() {
+        Some(("init", _)) => {
+            init()?;
+        }
+        Some(("hash-object", hash_object_matches)) => {
+            let pathspecs = hash_object_matches.values_of("pathspec").unwrap().collect::<Vec<_>>();
+            hash_object(pathspecs)?;
+        }
+        None => println!("No subcommand was used"),
+        _ => unreachable!(),
     }
 
     Ok(())
 }
 
-fn init() -> io::Result<()> {
-    data::init()
+fn init() -> Result<()> {
+    let retval = data::init();
+    println!(
+        "Initialized empty ugit repository in {}/{}",
+        env::current_dir()?.display(),
+        data::GIT_DIR
+    );
+    retval
+}
+
+fn hash_object(pathspecs: Vec<&str>) -> Result<()> {
+    data::hash_object(pathspecs)
 }
